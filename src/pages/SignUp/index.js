c37b56386b56b91ed4c7db448f9c4ff3cfc2b068
jmnsingle <jmnogueira2011@gmail.com>
2019-11-17 20:27:43 -0300

    Login e cadastro do app concluido.

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,9 +1,12 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Image } from 'react-native';
 
 import logo from '~/assets/logo.png';
 
 import Background from '~/components/Background';
+import { signUpRequest } from '~/store/modules/auth/actions';
+
 import {
   Container,
   Form,
@@ -14,10 +17,20 @@ import {
 } from './styles';
 
 export default function SignUp({ navigation }) {
+  const dispatch = useDispatch();
+
+  const loading = useSelector(state => state.auth.loading);
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const passwordRef = useRef();
   const emailRef = useRef();
 
-  function handleSubmit() {}
+  function handleSubmit() {
+    dispatch(signUpRequest(name, email, password));
+  }
 
   return (
     <Background>
@@ -32,6 +45,8 @@ export default function SignUp({ navigation }) {
             placeholder="Digite seu nome completo"
             returnKeyType="next"
             onSubmitEditing={() => emailRef.current.focus()}
+            value={name}
+            onChangeText={setName}
           />
           <FormInput
             icon="mail-outline"
@@ -42,6 +57,8 @@ export default function SignUp({ navigation }) {
             ref={emailRef}
             returnKeyType="next"
             onSubmitEditing={() => passwordRef.current.focus()}
+            value={email}
+            onChangeText={setEmail}
           />
 
           <FormInput
@@ -51,9 +68,13 @@ export default function SignUp({ navigation }) {
             ref={passwordRef}
             returnKeyType="send"
             onSubmitEditing={handleSubmit}
+            value={password}
+            onChangeText={setPassword}
           />
 
-          <SubmitButton onPress={() => {}}>Criar conta</SubmitButton>
+          <SubmitButton loading={loading} onPress={handleSubmit}>
+            Criar conta
+          </SubmitButton>
         </Form>
         <SignLink onPress={() => navigation.navigate('SignIn')}>
           <SignLinkText>Já tenho uma conta</SignLinkText>
---
+++ src/store/modules/user/sagas.js
@@ -0,0 +1,34 @@
+import { takeLatest, all, call, put } from 'redux-saga/effects';
+
+import { Alert } from 'react-native';
+
+import api from '~/services/api';
+
+import {
+  updateProfileSuccess,
+  updateProfileFailure,
+} from '~/store/modules/user/actions';
+
+function* updateProfile({ payload }) {
+  try {
+    const { name, email, avatar_id, ...rest } = payload.data;
+
+    const profile = {
+      name,
+      email,
+      avatar_id,
+      ...(rest.oldPassoword ? rest : {}),
+    };
+
+    const response = yield call(api.put, 'users', profile);
+
+    Alert.alert('Sucesso!', 'Dados atualizados com sucesso');
+
+    yield put(updateProfileSuccess(response.data));
+  } catch (err) {
+    Alert.alert('Erro ao atualizar perfil', 'Revise seus dados.');
+    yield put(updateProfileFailure());
+  }
+}
+
+export default all([takeLatest('@user/UPDATE_PROFILE_REQUEST', updateProfile)]);
